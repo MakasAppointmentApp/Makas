44001b20bdb82c0cfea93827159986f0b1ca2a35
Muhammedguven <mguvenn96@gmail.com>
2021-03-22 22:26:36 +0300

    Starting...

--- MakasUI/MakasUI/Views/WelcomePage.xaml.cs
+++ MakasUI/MakasUI/Views/WelcomePage.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace MakasUI
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MakasUI.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
     {
-        public MainPage()
+        public WelcomePage()
         {
             InitializeComponent();
         }

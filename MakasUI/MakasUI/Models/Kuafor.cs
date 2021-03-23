543262e2e6f2b10e4a17cc7e8e16621a53e5e48e
meorbag <mustafaemre456@gmail.com>
2021-03-24 02:56:57 +0300

    il seçme ve kuaför listeleme sayfası eklendi, back buttonlar değiştirildi shellPage tab ları hazırlandı DEVAAAAAMMMMM

---
+++ MakasUI/MakasUI/Models/Kuafor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakasUI.Models
+{
+    class Kuafor
+    {
+
+        public string kuaforPP { get; set; }
+
+        public string kuaforName { get; set; }
+
+        public string kuaforRate { get; set; }
+
+
+    }
+}

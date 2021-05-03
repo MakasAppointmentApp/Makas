29fc7522fb80c368f9e6f288fc48d8992bbf86eb
Muhammed Güven <mguvenn96@gmail.com>
2021-05-03 21:03:47 +0300

    Past and Future Appointments pages connectted API

--- MakasUI/MakasUI/Models/Worker.cs
+++ MakasUI/MakasUI/Models/Worker.cs
@@ -6,9 +6,20 @@ namespace MakasUI.Models
 {
     public class Worker
     {
+        public Worker()
+        {
+            //Reviews = new List<Review>();
+            Appointments = new List<Appointment>();
+        }
+        public int Id { get; set; }
+        public int SaloonId { get; set; }
         public string WorkerName { get; set; }
+        public byte[] WorkerPhoto { get; set; }
         public double WorkerRate { get; set; }
+        public Saloon Saloon { get; set; }
 
-        public string WorkerImage { get; set; }
+        //public List<Review> Reviews { get; set; }
+        public string WorkerImage { get; set; }//Bu gereksiz statik veriler bozulmasın diye duruyors silinecek
+        public List<Appointment> Appointments { get; set; }
     }
 }
---
+++ MakasUI/MakasUI/ViewModels/SaloonAuth/SaloonRegisterViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MakasUI.Models.DtosForAuth;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MakasUI.ViewModels.SaloonAuth
+{
+    public class SaloonRegisterViewModel
+    {
+        const string API_URL = "https://192.168.1.24:45456/api/SaloonAuth/register";
+        public async Task<bool> InsertUser(SaloonForRegisterDto saloon)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(clientHandler);
+
+            string serializedObject = JsonConvert.SerializeObject(saloon);
+            HttpContent contentPost = new StringContent(serializedObject, Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync(API_URL, contentPost);
+                Console.WriteLine(response);
+                return true;
+            }
+            catch (TaskCanceledException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(new HttpResponseMessage());
+                return false;
+            }
+        }
+    }
+}
